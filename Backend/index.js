abad20a62a0b67a90d4c47ddb1d793ff7b1b1884
avinashSD03 <138232801+avinashSD03@users.noreply.github.com>
2024-02-16 17:04:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "expensetracker",
-  password: "Chidambar@03",
+  password: "your password",
   port: 5432,
 });
 db.connect();
